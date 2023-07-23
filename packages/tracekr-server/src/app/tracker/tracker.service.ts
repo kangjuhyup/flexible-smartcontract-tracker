8e142d12d8fcb784b7553e7ee1dd2f81ac1e890e
kangjuhyup <fog0510@gmail.com>
2023-07-23 15:23:40 +0900

    fix

--- packages/tracekr-server/src/app/tracker/tracker.service.ts
+++ packages/tracekr-server/src/app/tracker/tracker.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { LazyModuleLoader } from '@nestjs/core';
-import { EventRepository } from '@root/database/event/event.repository';
-import { WebsocketService } from '@root/websocket/websocket.service';
+import { EventRepository } from '@server/database/event/event.repository';
+import { WebsocketService } from '@server/websocket/websocket.service';
 
 @Injectable()
 export class TrackerService {
@@ -14,13 +14,13 @@ export class TrackerService {
     ){}
 
     async loadDatabase() {
-        const { DatabaseModule } = await import('@database/database.module')
+        const { DatabaseModule } = await import('@server/database/database.module')
         const moudle_ref = await this.lazy_loader.load(() => DatabaseModule)
         this.repository = moudle_ref.get(EventRepository)
     }
 
     async loadSmartContract() {
-        const { WebSocketModule } = await import('@websocket/websocket.module')
+        const { WebSocketModule } = await import('@server/websocket/websocket.module')
         const module_ref = await this.lazy_loader.load(() => WebSocketModule)
         this.ws = module_ref.get(WebsocketService)
     }
